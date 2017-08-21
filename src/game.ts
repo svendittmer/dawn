758a99916aaa48bc2474faed0ad238aabee938f9
Sven Dittmer <sven.dittmer@gmx.net>
2017-08-21 12:49:48 +0200

    make demo more realistic
    - use a world map as ground
    - place the pirate ship in the ocean

--- src/game.ts
+++ src/game.ts
@@ -24,23 +24,32 @@ class Game {
     // attach the camera to the canvas
     this._camera.attachControl(this._canvas, false);
 
+    // this._scene.registerBeforeRender(() => {
+    //   this._scene.activeCamera.position.y = 10;
+    // });
+
     // create a basic light, aiming 0,1,0 - meaning, to the sky
     this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
 
     // create a built-in "ground" shape
     let ground = BABYLON.MeshBuilder.CreateGround('ground1',
-      { width: 6, height: 6, subdivisions: 2 }, this._scene);
+      { width: 64, height: 64, subdivisions: 2 }, this._scene);
 
     // apply a texture to the ground
     let materialGround = new BABYLON.StandardMaterial("texturePlane", this._scene);
-    materialGround.diffuseTexture = new BABYLON.Texture("textures/grass.jpg", this._scene);
+    materialGround.diffuseTexture = new BABYLON.Texture("textures/worldmap.png", this._scene);
     // materialGround.diffuseTexture.uScale = 5.0; // Repeat 5 times on the Vertical Axes
     // materialGround.diffuseTexture.vScale = 5.0; // Repeat 5 times on the Horizontal Axes
     materialGround.backFaceCulling = false; // Always show the front and the back of an element
     ground.material = materialGround;
 
     BABYLON.SceneLoader.ImportMesh("", "meshes/", "pirate-ship.babylon", this._scene,
-      function(newMeshes, particleSystems, skeletons) {}
+      function(newMeshes, particleSystems, skeletons) {
+        let ship = newMeshes[0];
+        newMeshes[1].setParent(ship);
+        newMeshes[2].setParent(ship);
+        ship.position.x = -6;
+      }
     );
 
   }
